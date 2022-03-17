d09d65a852b801e52468e7532f2dbfcc6f6e71cd
NikolasGumenyuk <nikos220599@gmail.com>
2022-03-17 10:09:04 +0200

    add sorting

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -5,18 +5,42 @@ import { Headers } from "./Headers";
 import { Body } from "./Body";
 
 export const BasicTable = () => {
+  const [data, setData] = React.useState(MOCK_DATA.slice(0, 10));
   const columns = COLUMNS;
-  const data = MOCK_DATA.slice(0, 10);
+  // const data = MOCK_DATA.slice(0, 10);
+  let sortedData = data;
+
+  const sortData = (fieldName, direction) => {
+    // console.log(fieldName, direction);
+    const sortDownFunc = (a, b) =>
+      a[fieldName] > b[fieldName] ? 1 : b[fieldName] > a[fieldName] ? -1 : 0;
+
+    const sortUpFunc = (a, b) =>
+      a[fieldName] < b[fieldName] ? 1 : b[fieldName] < a[fieldName] ? -1 : 0;
+
+    const selectedDirection =
+      direction === "down" ? sortDownFunc : sortUpFunc;
+
+    setData(sortedData.slice().sort(selectedDirection));
+
+    // console.log(sortedData);
+  };
 
   return (
     <table>
       <thead>
         <tr key="header">
-        { columns.map(column => <Headers key={column.header} column={column}/>) }
+          {columns.map((column) => (
+            <Headers key={column.header} column={column} onClick={sortData} />
+          ))}
         </tr>
       </thead>
       <tbody>
-        { data.map((item, index) => <tr key={index}><Body columns={columns} item={item}/></tr>)}
+        {data.map((item, index) => (
+          <tr key={index}>
+            <Body columns={columns} item={item} />
+          </tr>
+        ))}
       </tbody>
     </table>
   );
--- src/components/Headers.js
+++ src/components/Headers.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 export function Headers(props) {
-    const name = props.column.header;
+  const name = props.column.header;
+
   return (
     <th>
       {name}
+     
+      <button onClick={() => props.onClick(props.column.accessor, 'down')}>
+      Down
+      </button>
+      <button onClick={() => props.onClick(props.column.accessor, 'up')}>
+      Up
+      </button>
     </th>
   );
 }

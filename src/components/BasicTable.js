589f725b0221070986d5e4b4671ad19ef7dfb817
NikolasGumenyuk <nikos220599@gmail.com>
2022-03-27 16:06:10 +0300

    fix something

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS } from "./columns";
 import { Headers } from "./Headers";
